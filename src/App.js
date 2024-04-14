c9aebfc63af6158758b2f5888994c4e8e6cb4e30
ThibautDelporte98 <thibaut.delporte@student.howest.be>
2024-04-14 20:38:09 +0200

    Seo HOME

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 import "./App.css";
 import Nav from "./components/nav/nav.js";
 import HeaderLarge from "./components/page-title/header-large";
@@ -13,28 +14,235 @@ import Footer from "./components/footer/Footer";
 import Cta from "./components/call-to-actions/cta";
 import CookieAcceptance from "./components/cookies/cookie";
 
-
 function App() {
+  const keywords = [
+    "Schilder en decoratiewerken",
+    "Schilder inhuren",
+    "Binnen- en buitenschilderwerk",
+    "Professionele schilders",
+    "Schilder offerte",
+    "Muurschilderingen",
+    "Kleuradvies schilderen",
+    "Decoratief schilderwerk",
+    "Schilderwerk renovatie",
+    "Particuliere schilder",
+    "Zakelijke schilder",
+    "Schilders en onderhoudsbedrijf",
+    "Decoratieve technieken",
+    "Interieur schilderen",
+    "Exterieur schilderen",
+    "schilderbedrijf",
+    "schilder",
+    "verf",
+    "schilderwerk",
+  ];
 
-  
+  const websiteSchema = {
+    "@context": "http://schema.org",
+    "@type": "WebSite",
+    name: "Decoratie Degezelle",
+    description: "Uw partner voor hoogwaardige decoratieve schildersdiensten",
+    url: "https://decoratiedegezelle.be/",
+    sameAs: [
+      "https://www.facebook.com/people/Decoratie-Degezelle/100090360757782/",
+      "https://www.instagram.com/decoratie_degezelle/",
+    ],
+    about: {
+      "@type": "Organization",
+      name: "Decoratie Degezelle",
+      description:
+        "Uw betrouwbare partner voor decoratieve schildersdiensten met passie voor vakmanschap en creativiteit.",
+      address: {
+        "@type": "PostalAddress",
+        streetAddress: "Kouterstraat 42",
+        addressLocality: "Harelbeke",
+        postalCode: "8530",
+        addressCountry: "BE",
+      },
+      email: "info@decoratiedegezelle.be",
+      telephone: "+32 478 366 070",
+      sameAs: [
+        "https://www.facebook.com/people/Decoratie-Degezelle/100090360757782/",
+        "https://www.instagram.com/decoratie_degezelle/",
+      ],
+    },
+    brand: {
+      "@type": "Brand",
+      name: "Decoratie Degezelle",
+      description:
+        "Uw partner voor hoogwaardige decoratieve schildersdiensten met passie voor vakmanschap en creativiteit.",
+      logo: "https://decoratiedegezelle.be/Thumbnail.webp",
+    },
+    slogan: "Verf je wereld binnen en buiten!",
+    potentialAction: {
+      "@type": "SearchAction",
+      target: "https://decoratiedegezelle.be/search?q={search_term_string}",
+      "query-input": "required name=search_term_string",
+      "@type": "OrganizeAction",
+      target: {
+        "@type": "EntryPoint",
+        urlTemplate: "https://decoratiedegezelle.be/contact",
+        inLanguage: "nl-BE",
+        actionPlatform: [
+          "http://schema.org/DesktopWebPlatform",
+          "http://schema.org/IOSPlatform",
+          "http://schema.org/AndroidPlatform",
+        ],
+      },
+      expectsAcceptanceOf: {
+        "@type": "Offer",
+        priceSpecification: {
+          "@type": "CompoundPriceSpecification",
+          priceComponent: [
+            {
+              "@type": "UnitPriceSpecification",
+              name: "Toegangsprijs",
+              price: "0",
+              priceCurrency: "EUR",
+              valueAddedTaxIncluded: "false",
+              referenceQuantity: {
+                "@type": "QuantitativeValue",
+                value: 1,
+                unitCode: "MON",
+              },
+            },
+          ],
+        },
+      },
+    },
+    kernwaarden: [
+      {
+        "@type": "PropertyValue",
+        name: "Kwaliteit",
+        value: "Hoogwaardige kwaliteit staat bij ons voorop.",
+      },
+      {
+        "@type": "PropertyValue",
+        name: "Service",
+        value: "Wij bieden uitmuntende service aan onze klanten.",
+      },
+      {
+        "@type": "PropertyValue",
+        name: "Vertrouwen",
+        value: "Vertrouwen is de basis van onze relaties met klanten.",
+      },
+      {
+        "@type": "PropertyValue",
+        name: "Klantgericht",
+        value:
+          "Onze dienstverlening is volledig gericht op de behoeften van onze klanten.",
+      },
+      {
+        "@type": "PropertyValue",
+        name: "Flexibel",
+        value:
+          "Wij passen ons aan en zijn flexibel om aan de wensen van onze klanten te voldoen.",
+      },
+    ],
+  };
+
+  const breadcrumbListSchema = {
+    "@context": "http://schema.org",
+    "@type": "BreadcrumbList",
+    itemListElement: [
+      {
+        "@type": "ListItem",
+        position: 1,
+        item: {
+          "@id": "https://decoratiedegezelle.be/",
+          name: "Home",
+        },
+      },
+      {
+        "@type": "ListItem",
+        position: 2,
+        item: {
+          "@id": "https://decoratiedegezelle.be/diensten",
+          name: "Diensten",
+        },
+      },
+      {
+        "@type": "ListItem",
+        position: 3,
+        item: {
+          "@id": "https://decoratiedegezelle.be/realisaties",
+          name: "Referenties",
+        },
+      },
+      {
+        "@type": "ListItem",
+        position: 4,
+        item: {
+          "@id": "https://decoratiedegezelle.be/overons",
+          name: "Over ons",
+        },
+      },
+      {
+        "@type": "ListItem",
+        position: 5,
+        item: {
+          "@id": "https://decoratiedegezelle.be/contact",
+          name: "Offerte aanvragen",
+        },
+      },
+    ],
+  };
 
   return (
     <>
-      {/* <CookieAcceptance /> */}
-      <Nav />
-      <HeaderLarge
-        title="schilder & decoratiewerken"
-        subtitle="Verf je wereld binnen & buiten!"
-        text="Stap binnen en ervaar de kunst van schilderen bij Decoratie Degezelle. Wij transformeren niet alleen muren, maar creëren een levendige sfeer die jouw stijl weerspiegelt. Ontdek onze expertise, laat je inspireren en geef kleur aan jouw wereld."
-        color="transparent"
-        showCarousel={true}
-      />
-      <About />
-      <Values items={DataValues} />
-      <ServiceOverview items={DataProjects} />
-      <References items={DataRefs} />
-      <Cta />
-      <Footer />
+      <HelmetProvider>
+        {/* <CookieAcceptance /> */}
+
+        <Helmet>
+          <title>
+            Schilder & Decoratiewerken | Decoratie Degezelle (Schilderwerk in
+            jouw regio!)
+          </title>
+          <meta
+            name="description"
+            content="Ontdek een uitgebreid overzicht van onze diensten en bekijk onze recente projecten. Laat ons vakmanschap en toewijding jouw ruimte transformeren naar iets buitengewoons."
+          />
+          <meta name="keywords" content={keywords} />
+          <meta name="author" content="Alexandro Degezelle" />
+          <meta
+            property="og:title"
+            content="Decoratie Degezelle | Harelbeke | Facebook"
+          />
+          <meta
+            property="og:description"
+            content="Bij Decoratie Degezelle streven we naar perfectie in alles wat we doen. Als een gespecialiseerd bedrijf in decoratietechnieken hebben we ons volledig toegewijd om uw ruimtes te transformeren en een stijlvolle sfeer te creëren die uw persoonlijkheid weerspiegelt. Ons team van ervaren vakmensen gelooft in de kracht van samenwerken. We luisteren aandachtig naar uw wensen en ideeën en werken nauw met u samen om uw visie tot leven te brengen."
+          />
+          <meta
+            property="og:image"
+            content="https://www.facebook.com/photo/?fbid=105027762519249&set=a.103201362701889"
+          />
+          <meta
+            property="og:url"
+            content="https://www.facebook.com/people/Decoratie-Degezelle/100090360757782/"
+          />
+          <link rel="canonical" href="https://decoratiedegezelle.be/" />
+          <script type="application/ld+json">
+            {JSON.stringify(breadcrumbListSchema)}
+          </script>
+          <script type="application/ld+json">
+            {JSON.stringify(websiteSchema)}
+          </script>
+        </Helmet>
+        <Nav />
+        <HeaderLarge
+          title="schilder & decoratiewerken"
+          subtitle="Verf je wereld binnen & buiten!"
+          text="Stap binnen en ervaar de kunst van schilderen bij Decoratie Degezelle. Wij transformeren niet alleen muren, maar creëren een levendige sfeer die jouw stijl weerspiegelt. Ontdek onze expertise, laat je inspireren en geef kleur aan jouw wereld."
+          color="transparent"
+          showCarousel={true}
+        />
+        <About />
+        <Values items={DataValues} />
+        <ServiceOverview items={DataProjects} />
+        <References items={DataRefs} />
+        <Cta />
+        <Footer />
+      </HelmetProvider>
     </>
   );
 }
